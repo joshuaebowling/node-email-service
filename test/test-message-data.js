4dc84f9283c1f8537650bb7e44c8c3cc20f321b0
Josh Bowling <joshuaebowling@gmail.com>
2018-05-22 01:12:00 +0000

    added test data skeleton

---
+++ test/test-message-data.js
@@ -0,0 +1,20 @@
+module.exports = {
+  transport: {
+    host: '',
+    port: 587,
+    secure: true,
+    auth: {
+      user: '',
+      pass:''
+    }
+  },
+  to: [''],
+  messages: [
+    {
+      subject: '',
+      text: '',
+      html:''
+    }
+  ]
+
+};
