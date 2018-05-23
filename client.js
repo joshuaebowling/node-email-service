b19ab4c2b696a39c9cf34e50c70ff3e2a8efeaab
Josh Bowling <joshuaebowling@gmail.com>
2018-05-23 04:16:55 +0000

    client's sendMail method needs to behave differently in the event that the socket's connection is refused. I still want to keep the Clientinstance frozen and sealed but I need a way to keep the connection state :/

--- client.js
+++ client.js
@@ -6,8 +6,15 @@ const
 
 module.exports = function Client(host, port) {
   const self = this;
-  self.client = new JsonSocket(net.createConnection({host, port}));
+  self.connected = false;
+  self.client = new JsonSocket(net.createConnection({host, port}, () => {
+    self.connected = true;
+  }));
+  self.client.on('error', function(err) {
+    self.connected = false;
+  });
   self.sendEmail = function Client_sendEmail(transportInfo, from, to, subject, text, html) {
+    if(!self.connected) throw new Error('Connection Error');
     return new Promise((resolve, reject) => {
       self.client.on('message', (response) => {
         if(response.error) return reject(response.error);
@@ -15,6 +22,6 @@ module.exports = function Client(host, port) {
       });
       self.client.sendMessage({transportInfo, from, to, subject, text, html});
     });
-  };
+  };  
   return Object.freeze(Object.seal(self));
 };
